968c982e7fd09e6a270a69534629e046506145e6
trkuan <tianruei.kuan@gmail.com>
2018-12-02 01:29:37 -0600

    nav with auth

--- module2penguin/src/App.vue
+++ module2penguin/src/App.vue
@@ -1,12 +1,13 @@
 <template>
   <div id="app">
-    <ul id="nav" class="nav nav-pills justify-content-center">
+    <ul id="nav" class="nav nav-pills">
       <li class="nav-item">
         <router-link to="/map" class="nav-link">Map</router-link>
       </li>
       <li class="nav-item">
         <router-link to="/profile" class="nav-link">Profile</router-link>
       </li>
+      <li class="d-none d-md-block"><auth class="auth"/></li>
     </ul>
     <router-view :key="$route.fullPath" id = "page_contents"/>
   </div>
@@ -46,14 +47,30 @@ import { auth } from "@/firebaseConfig";
 </script>
 
 <style>
-#app {
+/*general setting*/
+* {
+    box-sizing: border-box;
+}
+html, body {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
 }
-
+/*nav bar*/
+#nav {
+  width: 100%;
+  z-index: 1;
+  background-color: white;
+  justify-content: center;
+  align-items: center;
+}
 @media (max-width: 991.98px/*md*/) {
+  #nav {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+  }
   #nav li{
     flex-grow: 1;
     flex-shrink: 1;
@@ -61,6 +78,5 @@ import { auth } from "@/firebaseConfig";
     text-align: center;
   }
 }
-
 
 </style>
