88f02d7cec5786fe3ae1b3db0b48a60a8602eca7
trkuan <tianruei.kuan@gmail.com>
2018-12-02 04:31:22 -0600

    city

---
+++ module2penguin/src/components/CityInfo.vue
@@ -0,0 +1,54 @@
+<template lang="html">
+  <div class="city-info">
+      <div class="city-img"></div>
+      <p class="city-name">Minneapolis</p>
+      <div class="star">
+        <font-awesome-icon icon="star" class="star-icon"/>114
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityInfo'
+}
+</script>
+
+<style lang="css" scoped>
+.city-info {
+  display: flex;
+  width: 100%;
+  flex-flow: column;
+  align-items: center;
+}
+.city-img {
+  width: 100%;
+  height: 350px;
+  background-image: url(https://www.roadtrips.com/wp-content/uploads/2017/12/minneapolis.jpg);
+  background-repeat: no-repeat;
+  background-position: top;
+  background-attachment: fixed;
+  background-clip: border-box;
+  background-size: 100% auto;
+}
+@media (max-width: 767px/*sm*/) {
+  .city-img {
+    background-size: 200% auto;
+    height: 150px;
+  }
+}
+.city-name {
+  font-size: 2rem;
+  margin-bottom: 0;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+div.star {
+  white-space: nowrap;
+  overflow: hidden;
+}
+.star-icon {
+  color: yellow;
+}
+</style>
--- module2penguin/src/views/City.vue
+++ module2penguin/src/views/City.vue
@@ -1,19 +1,42 @@
 <template lang="html">
   <div class="city">
+    <city-info />
+    <br />
     <div class="place-list list-group">
       <place-list-item placeName="Place1" visiteDate="9-11-2018"/>
       <place-list-item placeName="Place2" visiteDate="2-21-2018"/>
+      <place-list-item placeName="Place3" visiteDate="12-27-2017"/>
+      <place-list-item placeName="Place3" visiteDate="12-27-2017"/>
+      <place-list-item placeName="Place3" visiteDate="12-27-2017"/>
+      <place-list-item placeName="Place3" visiteDate="12-27-2017"/>
+      <place-list-item placeName="Place3" visiteDate="12-27-2017"/>
+      <place-list-item placeName="Place3" visiteDate="12-27-2017"/>
+      <place-list-item placeName="Place3" visiteDate="12-27-2017"/>
+      <place-list-item placeName="Place3" visiteDate="12-27-2017"/>
+      <place-list-item placeName="Place3" visiteDate="12-27-2017"/>
+      <place-list-item placeName="Place3" visiteDate="12-27-2017"/>
+      <place-list-item placeName="Place3" visiteDate="12-27-2017"/>
+      <place-list-item placeName="Place3" visiteDate="12-27-2017"/>
+      <place-list-item placeName="Place3" visiteDate="12-27-2017"/>
+      <place-list-item placeName="Place3" visiteDate="12-27-2017"/>
+      <place-list-item placeName="Place3" visiteDate="12-27-2017"/>
+      <place-list-item placeName="Place3" visiteDate="12-27-2017"/>
+      <place-list-item placeName="Place3" visiteDate="12-27-2017"/>
+      <place-list-item placeName="Place3" visiteDate="12-27-2017"/>
+      <place-list-item placeName="Place3" visiteDate="12-27-2017"/>
       <place-list-item placeName="Place3" visiteDate="12-27-2017"/>
     </div>
   </div>
 </template>
 
 <script>
+import CityInfo from '@/components/CityInfo.vue'
 import PlaceListItem from '@/components/PlaceListItem.vue'
 export default {
   name: 'City',
   components: {
-    PlaceListItem
+    PlaceListItem,
+    CityInfo
   }
 }
 </script>
