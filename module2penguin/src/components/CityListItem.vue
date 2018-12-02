9f5644157cf1889f4e77d4d05eda259150114bee
trkuan <tianruei.kuan@gmail.com>
2018-12-02 03:03:24 -0600

    profile page

---
+++ module2penguin/src/components/CityListItem.vue
@@ -0,0 +1,41 @@
+<template lang="html">
+  <router-link :to="{name:'city', params:{city:cityName}}" class="list-group-item" exact>
+    <div class="name">
+      {{cityName}}
+    </div>
+    <div class="star">
+      <font-awesome-icon icon="star" class="star-icon"/>{{startNum}}
+    </div>
+  </router-link>
+</template>
+
+<script>
+export default {
+  name: 'CityListItem',
+  props: {
+    cityName: String,
+    startNum: Number
+  }
+}
+</script>
+
+<style lang="css" scoped>
+.list-group-item {
+  display: flex;
+  justify-content: space-between;
+}
+div.name {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+div.star {
+  width: 4rem;
+  flex-shrink: 0;
+  white-space: nowrap;
+  overflow: hidden;
+}
+.star-icon {
+  color: yellow;
+}
+</style>
---
+++ module2penguin/src/components/UserInfo.vue
@@ -0,0 +1,31 @@
+<template lang="html">
+  <div class="user-info">
+      <img class="profile-img" src="https://image.flaticon.com/icons/svg/1338/1338023.svg" alt="profile image">
+        <p class="user-name">Username</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserInfo'
+}
+</script>
+
+<style lang="css" scoped>
+.user-info {
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+.profile-img {
+  border-radius: 50%;
+  width: 150px;
+  height: 150px;
+}
+.user-name {
+  font-size: 2rem;
+}
+</style>
