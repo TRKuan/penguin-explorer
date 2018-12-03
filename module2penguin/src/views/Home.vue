e1f82b46bbe07a47c6794d588377ac194119b2cd
trkuan <tianruei.kuan@gmail.com>
2018-12-02 20:59:23 -0600

    add auth to home

--- module2penguin/src/views/Home.vue
+++ module2penguin/src/views/Home.vue
@@ -2,16 +2,20 @@
   <div class="home">
   <h2>Katie's To-do App</h2>
   <h3>login to get started</h3>
+  <auth />
 
   </div>
 </template>
 
 <script>
+import Auth from "@/components/Auth.vue";
 import { auth, provider } from "@/firebaseConfig";
 
 export default {
   name: 'home',
-
+  components: {
+    Auth
+  },
   data() {
     return {
       user: null
