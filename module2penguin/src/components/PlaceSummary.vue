4e768e911e5f4eec672b338e7e997ff8c43a1d96
Katie Watson <katie.m.watson@gmail.com>
2018-12-04 11:10:18 -0600

    added basic componenet for place

---
+++ module2penguin/src/components/PlaceSummary.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+  Place Summary
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PlaceSummary",
+  data() {
+    return {
+      placeName: "Place Name",
+      PlaceAddress: "PlaceAddress",
+      City: "Minneapolis",
+      Visited: false,
+      OnWishlist: true,
+
+    };
+  },
+
+
+
+  methods: {
+
+  }
+};
+</script>
